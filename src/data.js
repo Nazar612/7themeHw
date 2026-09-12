083ce8271f00cbc5c1bb530bf413d25f1432aea7
Nazar612 <nazarstovba300@gmail.com>
2026-09-12 14:35:07 +0200

    3

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+import template from "./templates/products.hbs";
 export const products = [
   {
     id: 1,
@@ -18,3 +19,34 @@ export const products = [
     description: 'Noise-cancelling headphones for better focus.',
   },
 ];
+
+const zakladki = {
+    zaklInp: document.querySelector("#bookmarkInput"),
+    zaklButton: document.querySelector("#addBookmarkBtn"),
+    zaklList: document.querySelector("#bookmarkList"),
+};
+zakladki.zaklList.innerHTML = template({products});
+let index = 0;
+let zakladka = "zakladka" + index;
+
+// zakladki.zaklButton.addEventListener("click", (e)=> {
+//     let value = zakladki.zaklInp.value;
+//     if(value !== ""){
+//         index++;
+//         zakladka = "zakladka" + index;
+//         sessionStorage.setItem(zakladka ,value)
+//         console.log(        sessionStorage.setItem(zakladka ,value)
+// );
+//     }
+// })
+
+zakladki.zaklList.addEventListener("click", (e)=>{
+    if(!e.target.classList.contains("item")) {
+        return
+    };
+    console.log(e.target.id);
+    sessionStorage.setItem(e.target.id, JSON.stringify(e.target))
+    console.log(JSON.parse(sessionStorage.getItem(e.target.id)));
+})
+
+console.log(products[1].id);
